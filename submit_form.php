b43f777586ce60f77a6f903cc7dccde87509a99a
allesandroya <113670008+allesandroya@users.noreply.github.com>
2023-09-07 14:36:02 +0700

    Add files via upload
    Initial Commit

---
+++ submit_form.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Your email address
+    $recipient = "allesandroya@gmail.com";
+
+    // Form field values
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Email subject
+    $subject = "New Message from $name";
+
+    // Email body
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Message: $message";
+
+    // Email headers
+    $headers = "From: $name <$email>";
+
+    // Send the email
+    if (mail($recipient, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Message could not be sent.";
+    }
+}
+?>
